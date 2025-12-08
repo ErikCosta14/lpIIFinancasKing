e695dcc1aec304592e8a0e15fc0c021c26f00c7a
ErikCosta14 <dg09662@gmail.com>
2025-12-08 20:52:41 -0300

    Tela de Financas pronta para adicionar informações variáveis e Styles da tela de Financas completo

--- src/views/Transacao.js
+++ src/views/Transacao.js
@@ -1,10 +1,36 @@
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { useState } from "react";
 
-export default function Transacao({id, tipo, valor, descricao}) {
+import styles from '../styles/styles';
+
+export default function Transacao({id, tipo, valor, nome, descricao}) {
+    const [receita, setReceita] = useState(tipo == 'Receita' ? true : false);
+    const colorTipo = receita ? '#81cc2a' : '#cc2a2a';
+    const estilo = StyleSheet.create({
+        tipoReceita: {
+            color: colorTipo,
+        },
+    });
+
     return (
-        <View>
-            <Text>Transação {id}</Text>
-            <Text>{tipo}: R$ {valor} - {descricao}</Text>
+        <View style={styles.verFin.views}>
+            <View style={styles.verFin.viewValores}>
+                <Text style={styles.verFin.text}>{nome}</Text>
+                <Text style={[estilo.tipoReceita, styles.verFin.val]}>{receita ? "+" : "-"}R${valor}</Text>
+            </View>
+            
+            <View>
+                <Text style={styles.verFin.textDesc}>{descricao}</Text>
+            </View>
+
+            <View style={styles.verFin.buttons}>
+                <TouchableOpacity style={styles.verFin.buttonEdit} onPress={() => {}}>
+                    <Text style={styles.text2}>Editar</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.verFin.buttonDelete} onPress={() => {}}>
+                    <Text style={styles.text1}>Excluir</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
